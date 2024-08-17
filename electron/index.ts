59587fd2e729378a6b35cbc20bac027681cdab17
cooluid <400822@163.com>
2024-08-17 18:30:19 +0800

    一些修改

--- electron/index.ts
+++ electron/index.ts
@@ -1,72 +1,67 @@
-import { app, BrowserWindow, ipcMain } from 'electron'
-import { dialog } from 'electron/main';
+import {app, BrowserWindow, ipcMain} from 'electron'
+import {dialog} from 'electron/main';
 import path from 'path'
+import IpcMainInvokeEvent = Electron.IpcMainInvokeEvent;
 
 function createWindow() {
-    const isDev = process.env.IS_DEV === "true";
-    console.log("isDEV", isDev);
+	const isDev = process.env.IS_DEV === "true";
+	console.log("isDEV", isDev);
 
-    let win: BrowserWindow | null;
+	let win: BrowserWindow | null;
 
-    if (isDev) {
-        win = new BrowserWindow({
-            width: 800,
-            height: 600,
-            webPreferences: {
-                contextIsolation: true,
-                nodeIntegration: false,
-                allowRunningInsecureContent: true,
-                preload: path.join(__dirname, './preload.js')
-            }
-        });
+	if (isDev) {
+		win = new BrowserWindow({
+			width: 800,
+			height: 800,
+			webPreferences: {
+				contextIsolation: true,
+				nodeIntegration: false,
+				allowRunningInsecureContent: true,
+				preload: path.join(__dirname, './preload.js')
+			}
+		});
 
-        ipcMain.removeHandler('dialog:openDirectory');
-        ipcMain.handle("dialog:openDirectory", async (event, ...args: any) => {
-            console.log(`收到主进程异步操作消息`);
-            console.log(`win`, win);
-            const result = await dialog.showOpenDialog(win as BrowserWindow, { properties: ['openDirectory'] });
-            console.log(`获取异步操作结果：`, result);
-            return result.filePaths;
-        });
+		win.loadURL("http://localhost:5173");
 
-        win.webContents.closeDevTools();
-        win.webContents.openDevTools();
+	} else {
+		win = new BrowserWindow({
+			width: 800,
+			height: 800,
+			webPreferences: {
+				contextIsolation: true,
+				nodeIntegration: false,
+				allowRunningInsecureContent: false,
+				preload: path.join(__dirname, './preload.js')
+			}
+		});
 
-        win.loadURL("http://localhost:5173");
+		win.loadURL(`file://${path.resolve(__dirname, '../')}/dist/index.html`);
+	}
 
-    } else {
-        win = new BrowserWindow({
-            width: 800,
-            height: 600,
-            webPreferences: {
-                contextIsolation: true,
-                nodeIntegration: false,
-                allowRunningInsecureContent: false,
-                preload: path.join(__dirname, './preload.js')
-            }
-        });
+	win.webContents.openDevTools();
 
-        win.loadURL(`file://${path.resolve(__dirname,'../')}/dist/index.html`);
-    }
+	ipcMain.handle("dialog:openDirectory", async (evt: IpcMainInvokeEvent, ...args: any[]) => {
+		console.log(`收到渲染进程发来的消息dialog:openDirectory`, evt, ...args);
+		const result = await dialog.showOpenDialog(win as BrowserWindow, {properties: ['openDirectory']});
+
+		return result.filePaths;
+	});
 }
 
 app.whenReady().then(() => {
-    // 创建windows应用
-    createWindow();
+	// 创建windows应用
+	createWindow();
 
-    // 延迟3s 等待应用激活
-    setTimeout(() => {
-        console.log('已经过了3s了');
-        app.on('activate', function () {
-            // 如果应用激活后,窗口依然为0,则重新创建windows应用
-            if (BrowserWindow.getAllWindows().length === 0) createWindow();
-        });
-    }, 3000);
+	// 延迟3s 等待应用激活
+	setTimeout(() => {
+		console.log('已经过了3s了');
+		app.on('activate', function () {
+			// 如果应用激活后,窗口依然为0,则重新创建windows应用
+			if (BrowserWindow.getAllWindows().length === 0) createWindow();
+		});
+	}, 3000);
 });
 
 app.on('window-all-closed', () => {
-    if (process.platform !== "darwin") {
-        console.log(`退出APP`);
-        app.quit();
-    }
+	app.quit();
 })
