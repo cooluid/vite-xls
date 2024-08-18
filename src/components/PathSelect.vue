3c00d06338f2cb44fd693e32d50132d8ef727b2b
cooluid <400822@163.com>
2024-08-19 01:35:56 +0800

    显示文件列表

--- src/components/PathSelect.vue
+++ src/components/PathSelect.vue
@@ -8,12 +8,15 @@
 				placeholder="请选择表格配置路径"
 		/>
 		<button @click="btnClick">选择</button>
+		<XlsFileItem v-for="(name, index) in xlsFileName" :key="index" :index="index" :name="name"/>
 	</div>
 </template>
 <script setup lang="ts">
 import {ref} from "vue";
+import XlsFileItem from "./XlsFileItem.vue";
 
 let pathValue = ref("");
+let xlsFileName = ref("");
 
 const btnClick = async () => {
 	let selectPath = await window.electronAPI.invoke("dialog:openDirectory", {
@@ -22,10 +25,11 @@ const btnClick = async () => {
 
 	if (selectPath?.length > 0) {
 		const fileNames = await window.electronAPI.invoke("get-files-in-directory", selectPath[0]);
+		xlsFileName = fileNames;
 		console.log("fileNames", fileNames);
-	}
 
-	pathValue.value = selectPath[0];
+		pathValue.value = selectPath[0];
+	}
 };
 </script>
 <style scoped></style>
---
+++ src/components/XlsFileItem.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps({
+	name: String,
+	index: Number
+});
+
+</script>
+
+<template>
+	<div>
+		{{ index }}. {{ name }}
+	</div>
+</template>
+
+<style scoped>
+
+</style>
