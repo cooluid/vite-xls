50d983af9a8fa2545387303ef3b02d1c903ed444
cooluid <400822@163.com>
2024-08-19 02:42:27 +0800

    自定义属性

--- src/components/XlsFileItem.vue
+++ src/components/XlsFileItem.vue
@@ -1,8 +1,21 @@
 <script lang="ts" setup>
 interface Props {
-	index: number;
+	index: 0;
 	name: string;
 }
+
+defineProps({
+	id: Number,
+	ext: {
+		type: Object,
+		required: true,
+		default: {"num": 1},
+	}
+})
+
+defineProps({
+	type: [String]
+});
 
 const props = defineProps<Props>();
 
