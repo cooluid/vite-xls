b2a0cb366cb4553bcbf7455ffd2add1cfd460f6e
cooluid <400822@163.com>
2024-08-27 03:23:21 +0800

    表格结构

--- src/utils/XlsxUtil.ts
+++ src/utils/XlsxUtil.ts
@@ -35,32 +35,94 @@ function parseCell(cellValue: string): any {
   }
 }
 
+//根据列数和行数获取所有对应单元格的值
+function getCellValues(worksheet: WorkSheet, startRow: number, endRow: number, col: number): any[] {
+  const parsedDataList: any[] = [];
+
+  for (let rowIndex = startRow; rowIndex <= endRow; rowIndex++) {
+    const cellAddress = xlsx.utils.encode_cell({ r: rowIndex, c: col });
+    const cell = worksheet[cellAddress];
+    parsedDataList.push(cell ? parseCell(cell.v) : undefined);
+  }
+
+
+  return parsedDataList;
+}
+
+//根据行数和列数获取所有对应单元格的值
+function getCellValuesByRowAndCol(worksheet: WorkSheet, startCol: number, endCol: number, row: number): any[] {
+  const parsedDataList: any[] = [];
+
+  for (let colIndex = startCol; colIndex <= endCol; colIndex++) {
+    const cellAddress = xlsx.utils.encode_cell({ r: row, c: colIndex });
+    const cell = worksheet[cellAddress];
+    parsedDataList.push(cell ? parseCell(cell.v) : undefined);
+  }
+
+  return parsedDataList;
+}
+
+//根据某行格子获取值
+function getCellValue(worksheet: WorkSheet, row: number, col: number): any {
+  const cellAddress = xlsx.utils.encode_cell({ r: row, c: col });
+  const cell = worksheet[cellAddress];
+  return cell ? parseCell(cell.v) : undefined;
+}
+
 function parseWorksheet(worksheet: WorkSheet): { [key: string]: any } {
   const range = xlsx.utils.decode_range(worksheet['!ref'] || "");
-  const colCount = range.e.c - range.s.c + 1;
-  const rowCount = range.e.r - range.s.r + 1;
+  const colCount = range.e.c - range.s.c;
+  const rowCount = range.e.r - range.s.r;
 
-  const columnNames = Array.from({ length: colCount }, (_, j) => {
-    const cellAddress = xlsx.utils.encode_cell({ r: HEADER_ROW, c: j + 1 });
-    const cell = worksheet[cellAddress];
-    return cell ? cell.v : '';
-  });
+  //获取A2单元格值
+  const keyNum = getCellValue(worksheet, 1, 0);
+  const firstKeyValues = getCellValues(worksheet, 4, rowCount, 1);//[1,2,3,4,5,6,7,8,9,10]
+  //一层的数据
+  let map: { [key: string]: any } = {};
+  let propertyList = getCellValuesByRowAndCol(worksheet, 1, colCount, 2);//["a","b","c","d","e","f","g","h","i","j"];
+  let valueMap: { [key: string]: any } = {};
 
-  const parsedData: { [key: string]: any } = {};
+  for (let i = 0; i < firstKeyValues.length; i++) {
+    valueMap[i] = getCellValuesByRowAndCol(worksheet, 1, colCount, i + 4);
+  }
 
-  columnNames.forEach((name, colIndex) => {
-    if (!name) return;
-
-    const columnData: { [key: number]: any } = {};
-    for (let rowIndex = DATA_START_ROW; rowIndex < rowCount; rowIndex++) {
-      const cellAddress = xlsx.utils.encode_cell({ r: rowIndex, c: colIndex + 1 });
-      const cell = worksheet[cellAddress];
-      columnData[rowIndex - DATA_START_ROW + 1] = cell ? parseCell(cell.v) : undefined;
+  //创建键值对
+  for (let i = 0; i < firstKeyValues.length; i++) {
+    let key = firstKeyValues[i];
+    for (let j = 0; j < propertyList.length; j++) {
+      let p = propertyList[j];
+      let v = valueMap[i][j];
+      let o: { [key: string]: any } = {};
+      o[p] = v;
+      map[key] = { ...map[key], ...o };
     }
-    parsedData[name] = columnData;
-  });
+  }
 
-  return parsedData;
+  let endMap: { [key: string]: any } = {};
+  for (let i = keyNum; i > 0; i--) {
+
+  }
+  // const columnNames = Array.from({ length: colCount }, (_, j) => {
+  //   const cellAddress = xlsx.utils.encode_cell({ r: HEADER_ROW, c: j + 1 });
+  //   const cell = worksheet[cellAddress];
+  //   return cell ? cell.v : '';
+  // });
+
+  // const parsedData: { [key: string]: any } = {};
+
+  // columnNames.forEach((name, colIndex) => {
+  //   if (!name) return;
+
+  //   const columnData: { [key: number]: any } = {};
+  //   for (let rowIndex = DATA_START_ROW; rowIndex < rowCount; rowIndex++) {
+  //     const cellAddress = xlsx.utils.encode_cell({ r: rowIndex, c: colIndex + 1 });
+  //     const cell = worksheet[cellAddress];
+  //     columnData[rowIndex - DATA_START_ROW + 1] = cell ? parseCell(cell.v) : undefined;
+  //   }
+  //   parsedData[name] = columnData;
+  // });
+
+  return map;
 }
 
 export function parseWorkbook(workbookList: WorkBook[]): ParsedData | null {
